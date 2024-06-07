c52a66e74875c2d10c24a38a3c72ce571ab1558c
dandervich <48976961@est.ort.edu.ar>
2024-06-07 00:54:11 -0300

    checking serialports everytime we recieve an input

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,26 @@
+use serialport;
+
+// https://github.com/PyO3/pyo3
+// ^ investigate this option?
+
+// use ubuntu 22.04.3 LTS DONT FORGET
+
+// https://stackoverflow.com/questions/24214643/python-to-automatically-select-serial-ports-for-arduino
+// ^ this will help us find the port with the keyboard dynamically... only problem could be if another device has the same name then we're fucked LOL
+
+// opening and cofiguring a serial port
+// let port = serialport::new("/dev/ttyUSB0", 115_200)
+// .timeout(Duration::from_millis(10))
+// .open().expect("Failed to open port");
+
+// writing to a port
+// let output = "This is a test. This is only a test.".as_bytes();
+// port.write(output).expect("Write failed!");
+
+// reading from a port
+// let mut serial_buf: Vec<u8> = vec![0; 32];
+// let n = port.read(serial_buf.as_mut_slice()).expect("Read failed!");
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -5,15 +28,21 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command(rename_all = "snake_case")]
-fn my_custom_command(invoke_message: String) {
-  println!("YAAAA DAS WASAP: {}", invoke_message);
+fn current_input(ci: String) {
+    println!("the current input is: {}", ci);
+    let ports = serialport::available_ports().expect("No ports found!");
+    println!("ports: {}", ports.len());
+    for p in ports {
+        println!("port: {}", p.port_name);
+    }
+    println!("done");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet, my_custom_command])
+        .invoke_handler(tauri::generate_handler![greet, current_input])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
