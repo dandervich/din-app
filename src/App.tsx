ba90f41f7fee7a0a3a01bed6c308d4411500ab07
dandervich <64498426+dan-dervich@users.noreply.github.com>
2024-08-05 12:12:25 -0300

    added suggestions functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { AnchorHTMLAttributes, HTMLAttributeAnchorTarget, useEffect, useRef, useState } from "react";
 import "./App.css";
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
@@ -9,21 +9,20 @@ function App() {
   // }
   const [input, setInput] = useState("");
   const [layout, setLayout] = useState("default");
-  const [suggestions, setSuggestions] = useState([""]);
+  const [suggestions, setSuggestions] = useState(["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti."]);
   const keyboard = useRef();
 
   const getSuggestions = async (ct: string): Promise<string[]> => {
-    const res = await fetch("http://10.8.17.40:5000/autocomplete", {
+    const res = await fetch("http://10.8.17.10:5000/autocomplete", {
       method: "POST",
       body: JSON.stringify({texto: ct}),
-      mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
       }      
     })
     const data = await res.json()
-    console.log("data", data)
-    return data
+    console.log("data", data.text)
+    return data.text
   }
   console.log("sug", suggestions)
   const onChange = async (input: any) => {
@@ -32,7 +31,6 @@ function App() {
     let sug: Awaited<string[]> = await getSuggestions(input)
     setSuggestions(sug);
     console.log(sug)
-
   };
 
   const handleShift = () => {
@@ -55,6 +53,11 @@ function App() {
     }
   };
 
+  const predClick = (e: React.MouseEvent<HTMLElement>) => {
+    const el = e.target as HTMLElement
+    setInput(input + el.innerText);
+    
+  }
   const onChangeInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const input = event.target.value;
     setInput(input);
@@ -70,9 +73,11 @@ function App() {
         <div className="input-wrapper">
           <textarea className="input" onChange={onChangeInput} value={input}> </textarea>
           <div className="predictions">
-            <a className="prediction">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.</a>
-            <a className="prediction">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.</a>
-            <a className="prediction">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.</a>
+            {
+              suggestions.map((suggestion: string) => {
+                return <a onClick={predClick} className="prediction">{suggestion}</a>
+              })
+            }
           </div>
         </div>
         <Keyboard
