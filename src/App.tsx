4be6dc9191be95efa71e16e9401ebe9c0790fca2
dandervich <48976961@est.ort.edu.ar>
2024-10-15 00:21:13 -0300

    tic experience mvp : /

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,48 @@ function App() {
   // }
   const [input, setInput] = useState("");
   const [layout, setLayout] = useState("default");
-  const [suggestions, setSuggestions] = useState(["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti.", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quod sequi quam sit. Ab, tempore assumenda alias incidunt quam voluptatibus labore eius et autem perferendis veniam suscipit odio odit corrupti."]);
-  const keyboard = useRef(); 
+  const [secsuggestions, setSecSuggestions] = useState("hola");
+  const [suggestions, setSuggestions] = useState(["hola", "hola, todo bien", "hola, bien vos?"]);
+  const keyboard = useRef();
   const [selectedSug, setSelectedSug] = useState(0)
 
   const getSuggestions = async (ct: string): Promise<string[]> => {
-    const res = await fetch("http://10.4.16.172:3000/autocomplete", {
+    const res = await fetch("http://172.20.184.35:3000/autocomplete", {
       method: "POST",
       body: JSON.stringify({ texto: ct }),
       headers: {
         "Content-Type": "application/json",
       }
     })
-    console.log(res)
     const data = await res.json()
-    console.log("data", data)
     return data
   }
+  const inactivityTime = () => {
+    var time = 0;
+    window.onload = resetTimer;
+    // DOM Events
+    var events = ['mousedown', 'mousemove', 'keypress', 'scroll', 'touchstart'];
+    events.forEach(function (name) {
+      document.addEventListener(name, resetTimer, true);
+    });
+    function resetTimer() {
+      clearTimeout(time);
+      const gs = async () => {
+        let sug: Awaited<string[]> = await getSuggestions(input)
+        setSuggestions(sug);
+        setSecSuggestions(sug[0]);
+      }
+      time = setTimeout(gs, 3000)
+      // 1000 milliseconds = 1 second
+    }
+  };
   const onChange = async (input: any) => {
     setInput(input);
     console.log("Input changed", input);
     if (input.length % 3 == 0) {
       let sug: Awaited<string[]> = await getSuggestions(input)
       setSuggestions(sug);
+      setSecSuggestions(sug[0]);
     }
   };
 
@@ -46,7 +65,7 @@ function App() {
 
     /**
      * If you want to handle the shift and caps lock buttons
-     */
+    */
     if (button === "{shift}" || button === "{lock}") handleShift();
     // make0 an enter without using \n
     if (button === "{enter}") {
@@ -54,12 +73,20 @@ function App() {
       keyboard.current.setInput(input + "\n");
       setInput(input + "\n")
     }
+    if (button === "{bksp}") {
+      // @ts-ignore
+      keyboard.current.setInput(input.slice(0, -1));
+      setInput(input.slice(0, -1))
+    }
   };
 
   const predClick = (e: React.MouseEvent<HTMLElement>) => {
     const el = e.target as HTMLElement
     setSelectedSug(el.id as unknown as number)
-    setInput(input + suggestions[el.id as unknown as number])
+    // setInput(input + suggestions[el.id as unknown as number])
+    setInput(suggestions[el.id as unknown as number])
+    // @ts-ignore
+    keyboard.current.setInput(suggestions[el.id as unknown as number]);
   }
   const onChangeInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const input = event.target.value;
@@ -71,19 +98,42 @@ function App() {
     // leave suggestions the same but remove previous word completions manually
     let arr = suggestions[selectedSug].slice()
     let narr = arr.split(" ")
-    return narr[0]
+    let secArr = secsuggestions.slice().split(" ")
+    // remove the first word
+    if (secArr.length >= 1) {
+      secArr.shift()
+      setSecSuggestions(secArr.join())
+      return narr[0]
+    } else {
+      setSecSuggestions("")
+      return ""
+    }
   }
   useEffect(() => {
-    document.addEventListener("keydown", (e) => {
-      if (e.key == "Tab") {
-        e.preventDefault();
-        let word = tabAutCompletion()
-        console.log(input + word)
-        setInput(input + word + " ")
-        // @ts-ignore
-        keyboard.current.value += word
-      }
-    })
+    // document.addEventListener("mouseover", () => {
+    //   inactivityTime()
+    // })
+    // document.addEventListener("keydown", (e) => {
+    //   if (e.key == "Tab") {
+    //     e.preventDefault();
+    //     let word = tabAutCompletion()
+    //     // check if input doesn't have any spaces
+    //     if (!input.includes(" ")) {
+    //       setInput(word + " ")
+    //       // @ts-ignore
+    //       keyboard.current.value = word + " "
+    //     }
+    //     else if (input.split(" ")[input.split(" ").length - 1].includes(word)) {
+    //       setInput(input.split(" ").slice(0, -1).join(" ") + word + " ")
+    //       // @ts-ignore
+    //       keyboard.current.value = input.split(" ").slice(0, -1).join(" ") + word + " "
+    //     } else {
+    //       setInput(input + word + " ")
+    //       // @ts-ignore
+    //       keyboard.current.value += word
+    //     }
+    //   }
+    // })
   }, [input, suggestions])
   return (
     <div className="main-wrapper">
